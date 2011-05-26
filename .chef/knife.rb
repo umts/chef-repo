ce8953e670b33710c57a97e92f70e5a43d37f363
Matt Moretti <werebus@gmail.com>
2011-05-26 15:30:51 -0400

    New and improved knife.rb - now with ENV

---
+++ .chef/knife.rb
@@ -0,0 +1,17 @@
+current_dir =            File.dirname(__FILE__)
+user =                   ENV['OPSCODE_USER'] || ENV['USER']
+orgname =                ENV['ORGNAME'] || 'umts'
+
+log_level                :info
+log_location             STDOUT
+node_name                user
+client_key               "#{ENV['HOME']}/.chef/#{user}.pem"
+validation_client_name   "#{orgname}-validator"
+validation_key           "#{ENV['HOME']}.chef/#{orgname}-validatior.pem"
+chef_server_url          "https://api.opscode.com/organizations/#{orgname}"
+cache_type               'BasicFile'
+cache_options( :path =>  "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path [ "#{current_dir}/../cookbooks", "#{current_dir}/../site-cookbooks" ]
+
+cookbook_copyright       "UMass Transit Service"
+cookbook_email           "transit-mis@admin.umass.edu"
