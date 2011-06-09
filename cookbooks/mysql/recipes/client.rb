7e44362b68f7ff5dde8c111c23d64362f1d141c0
Matt Moretti <werebus@gmail.com>
2011-06-09 17:00:30 -0400

    Always use mysl gem for ruby-mysql

--- cookbooks/mysql/recipes/client.rb
+++ cookbooks/mysql/recipes/client.rb
@@ -32,21 +32,6 @@ package "mysql-devel" do
   action :install
 end
 
-if platform?(%w{ debian ubuntu redhat centos fedora suse })
-
-  package "mysql-ruby" do
-    package_name value_for_platform(
-      [ "centos", "redhat", "suse", "fedora"] => { "default" => "ruby-mysql" },
-      ["debian", "ubuntu"] => { "default" => 'libmysql-ruby' },
-      "default" => 'libmysql-ruby'
-    )
-    action :install
-  end
-
-else
-
-  gem_package "mysql" do
-    action :install
-  end
-
+gem_package "mysql" do
+  action :install
 end
