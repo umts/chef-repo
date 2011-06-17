bc1503463a51b8a7f033e00a14fd2b198e49b9ef
Matt Moretti <werebus@gmail.com>
2011-06-17 10:30:29 -0400

    Vagrant specific tweeks to round_two

--- cookbooks/round_two/recipes/vagrant.rb
+++ cookbooks/round_two/recipes/vagrant.rb
@@ -4,6 +4,37 @@
 
 include_recipe "apache2"
 
+app = data_bag_item("apps", "round_two")
+if app["database_master_role"]
+  dbm = nil
+  # If we are the database master
+  if node.run_list.roles.include?(app["database_master_role"][0])
+    dbm = node
+  else
+  # Find the database master
+    results = search(:node, "role:#{app["database_master_role"][0]} AND chef_environment:#{node.chef_environment}", nil, 0, 1)
+    rows = results[0]
+    if rows.length == 1
+      dbm = rows[0]
+    end
+  end
+
+  # Assuming we have one...
+  if dbm
+    template "/vagrant/config/database.yml" do
+      source "database.yml.erb"
+      mode "644"
+      variables(
+        :host => dbm['fqdn'],
+        :databases => app['databases'],
+        :rails_env => 'development'
+      )
+    end
+  else
+    Chef::Log.warn("No node with role #{app["database_master_role"][0]}, database.yml not rendered!")
+  end
+end
+
 web_app "round_two_vagrant" do
   docroot "/vagrant"
   template "round_two.conf.erb"
