7c538a585eaa3d278a8aeddb89140870fef92a14
Matt Moretti <werebus@gmail.com>
2011-06-17 14:13:38 -0400

    System-wide rvm is meant to be in /usr/local/rvm

--- cookbooks/rvm/files/default/rvm.sh
+++ cookbooks/rvm/files/default/rvm.sh
@@ -1,3 +1,1 @@
-# this doesn't work for me!
-# [[ -s "/usr/local/lib/rvm" ]] && . "/usr/local/lib/rvm"  # This loads RVM into a shell session.
-source /usr/local/lib/rvm
+[[ -s "/usr/local/rvm/scripts/rvm" ]] && . "/usr/local/rvm/scripts/rvm"  # This loads RVM into a shell session.
