c3047c61174c584e793c04e703c88fa75871e4bb
Matt Moretti <werebus@gmail.com>
2011-08-05 15:00:54 -0400

    Simple redhat-lsb recipe

---
+++ cookbooks/redhat-lsb/README.md
@@ -0,0 +1,5 @@
+Description
+===========
+This cookbook installs the redhat-lsb package on Redhat-based distros in
+order to allow ohai to access more specific relase information.  It also
+then asks ohai to repopulate these attributes.
---
+++ cookbooks/redhat-lsb/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "UMass Transit Service"
+maintainer_email "transit-mis@admin.umass.edu"
+license          "Apache 2.0"
+description      "Installs/Configures redhat-lsb"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+supports "redhat"
+supports "centos"
