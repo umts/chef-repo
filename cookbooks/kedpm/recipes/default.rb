acf24b1ea9e0e1988d9f4971e4df353285c05104
Matt Moretti <werebus@gmail.com>
2011-09-09 13:47:05 -0400

    Some small tweaks to kedpm

--- cookbooks/kedpm/recipes/default.rb
+++ cookbooks/kedpm/recipes/default.rb
@@ -20,6 +20,8 @@ require_recipe "python"
 
 kedpm_url = "http://sourceforge.net/projects/kedpm/files/kedpm/0.4.0/kedpm-0.4.0.tar.gz/download"
 
-["pycrypto", kedpm_url].each do |pkg|
-  easy_install_package pkg
-end
+easy_install_package "pycrypto"
+
+# It'd be nice to do this with easy_install_package like above, but the
+# proveider doesn't support urls or files as sources.  See CHEF-1695
+execute "easy_install #{kedpm_url}"
