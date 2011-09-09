4b14a65903706e162de3167079cbe57646539c8d
Matt Moretti <werebus@gmail.com>
2011-09-09 11:35:24 -0400

    KedPM recipe

---
+++ cookbooks/kedpm/recipes/default.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook Name:: kedpm
+# Recipe:: default
+#
+# Copyright 2011, UMass Transit Service
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+require_recipe "python"
+
+kedpm_url = "http://sourceforge.net/projects/kedpm/files/kedpm/0.4.0/kedpm-0.4.0.tar.gz/download"
+
+["pycrypto", kedpm_url].each do |pkg|
+  easy_install_package pkg
+end
