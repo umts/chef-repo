5c554f248b63b92faacd0a6ae3f2c393847c5d8c
Matt Moretti <werebus@gmail.com>
2011-09-23 12:16:45 -0400

    A start on the users data bag

---
+++ data_bags/users/matt.js
@@ -0,0 +1,9 @@
+{
+  "id": "matt",
+  "ssh_keys": "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAuzJRAfVvsbl2vvWVSxKd4vKgXIxNqFw+27h9TDO4jLTWNydCbKjQwE3cxHZK8nbYphYk+5Iovpr1w+99QUbBudFHoAYN3G5qKgO7+X5wnrIpXnZgY/9k5Kqk16oD6W5qEHeswuOj8S2Zr+x1L0pNsXitHz7Gt3d846ybHi7c0QWcFj95vsZ0MacgPUM8pa2HkNiqKXwkGg2DPfCfk+m0ndkYl3+1kS/4yj5PL2vpPk0ZddqrFsdg6vcoYzOvMygJ7MbPGKXuN+YQUFu+JdOSriV0iWy2Tn2niiaG5LfBO1HN8rxBAkNKtyFbOvuxeAtKJzbj+mgtUZ9FQUTeZ2qZXQ== matt",
+  "groups": "sysadmin",
+  "uid": 2001,
+  "shell": "\/bin\/bash",
+  "comment": "Matt Moretti",
+  "openid": "http://id.wereb.us"
+}
