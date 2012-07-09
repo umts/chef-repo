275f5384c8c653e8105af2a0a6a7536043020f99
Matt Moretti <werebus@gmail.com>
2012-07-09 11:54:13 -0400

    Moved to http://github.com/umts/chef-kedpm

--- cookbooks/kedpm/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer       "UMass Transit Service"
-maintainer_email "transit-mis@admin.umass.edu"
-license          "Apache 2.0"
-description      "Installs/Configures kedpm"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
-
-depends          "python"
