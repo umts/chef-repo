9c53fb7a32ada43ef706dd735517f1eb2b0a9f48
Matt Moretti <werebus@gmail.com>
2012-07-11 11:57:59 -0400

    Redmine role

---
+++ roles/redmine.rb
@@ -0,0 +1,15 @@
+name        "redmine"
+description "Sets up redmine with some setup tweaks"
+
+# These should get merged with the defaults in the cookbook
+default_attributes(
+  "redmine" => {
+    "revision" => "2.0.3",
+    "path" => "/srv/redmine",
+    "packages" => {
+      "scm" => []   #git is always installed
+    }
+  }
+)
+
+run_list "recipe[redmine]"
