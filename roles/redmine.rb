1a2cd7acf64e4e4e0591828fe45ef588dc07cc56
Matt Moretti <werebus@gmail.com>
2012-07-12 22:13:56 -0400

    Create a random app password
    The problem with this is that it generates a new one every time the role
    gets uploaded.  It doesn't break because the SQL fails to run, but the
    node data is no longer correct.  Probably best to switch to databags.

--- roles/redmine.rb
+++ roles/redmine.rb
@@ -1,11 +1,24 @@
+require 'openssl'
+
 name        "redmine"
 description "Sets up redmine with some setup tweaks"
+
+pw = String.new
+
+while pw.length < 20
+  pw << OpenSSL::Random.random_bytes(1).gsub(/\W/, '')
+end
 
 # These should get merged with the defaults in the cookbook
 override_attributes(
   "redmine" => {
     "revision" => "2.0.3",
     "path" => "/srv/redmine",
+    "databases" => {
+      "production" => {
+        "password" => pw
+      }
+    },
     "packages" => {
       "scm" => []   #git is always installed
     },
