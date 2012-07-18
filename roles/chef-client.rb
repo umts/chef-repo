3e6c301421d37a35f7f84598f411c5b65b99ec2a
Matt Moretti <werebus@gmail.com>
2012-07-18 14:31:03 -0400

    Removed override attributes from chef-client role
    init is now the default service type for Ubuntu, and the cookbook now
    looks at your knife config for server urls

--- roles/chef-client.rb
+++ roles/chef-client.rb
@@ -1,11 +1,6 @@
 name "chef-client"
 description "Base role applied to all nodes making them chef clients"
-override_attributes(
-  "chef_client" => {
-    "init_style" => "init",
-    "server_url" => "https://transit-chef.admin.umass.edu/"
-  }
-)
+
 run_list(
   "recipe[chef-client::delete_validation]",
   "recipe[chef-client::config]",
