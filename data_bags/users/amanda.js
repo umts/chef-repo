33b3467238d8a7a5c43c88cc6459dc017f9d148d
Matt Moretti <werebus@gmail.com>
2012-07-27 14:39:01 -0400

    Added sys-admin Amanda

---
+++ data_bags/users/amanda.js
@@ -0,0 +1,8 @@
+{
+  "id": "amanda",
+  "ssh_keys": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZ7IDb35QsjDsSPHMDWt9YgMALaMffyOsBO4vOHsTVxxcLQLCvDHnivFijr8aIsR2YGfPdOo/T8TFeXA1p34BCss8sTINf7B2/dbwV1QsNkvCpQuCp3HmkpoPoJeZUHXxObfoJfZ9sYDrmZLqpTuVR3PvOBd0qxkZ5kN8wN1rev4WYnIcoiGNak1QujWqPF4aS09A6kXrruepqntLGnsIsEKv6BhvdmoiIDEd9hhoP7CcFM8d0lKrRZVE9KxltUff38K9FgBX03Bysrwr4xB52AkodgAHF8W3hH7nfmcrreOuEPjforQEPTpi2ScVBSH8ddLPnkeDBZF9eGgXZW9px acadwellfrost@gmail.com",
+  "groups": "sysadmin",
+  "uid": 2002,
+  "shell": "\/bin\/bash",
+  "comment": "Amanda Cadwell-Frost"
+}
