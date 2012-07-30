bb76f6271e9ed029a15d526263e2d6acf50f3c6f
Matt Moretti <werebus@gmail.com>
2012-07-30 13:16:19 -0400

    Adam and Logan sysadmins

---
+++ data_bags/users/adam.js
@@ -0,0 +1,8 @@
+{
+  "id": "adam",
+  "ssh_keys": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDycHeCgF6QIBKGgXObQtwdQjddrKVLIYPVdC9/CYDFDKuD3wERChIZnxfP+5LpQrFvEwK8NFALhsiiPEc3gtfgOT3LstCJlV/BCxGARnwf2/drK4uygTjJF/IMA7/nvHySsdKxie4ZGd5gUn+fw97cgFSUNVzggtLWwNfpUX1iEIR9cnrxCvuZT3Xuj4UVPpd3KDd0JJhrbdbNfMYtuK2cw8yKN6+SpxGfsdf9+DaOT+j1su7woG3mFFgaPas5A7H2IkOjIsK/LvTC56Moi+R++uXcC+fHN7i2W/s5pUV0intKlb9GgkipFKuEIaqNArKzTh5yEKgbWkxS7GEJK2/P",
+  "groups": "sysadmin",
+  "uid": 2004,
+  "shell": "\/bin\/bash",
+  "comment": "Adam Sherson"
+}
