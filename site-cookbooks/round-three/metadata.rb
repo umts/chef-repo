75878b2ca716f2bca342b141d6ee4057d9cc7b5e
Matt Moretti <werebus@gmail.com>
2012-08-03 15:38:18 -0400

    Base application almost done
    TODO: put sym-link targets into shared - they're broken links currently.
    
    I also plan on separating the database and application code from
    each-other, with an eye on building a search-based system soon.

--- site-cookbooks/round-three/metadata.rb
+++ site-cookbooks/round-three/metadata.rb
@@ -5,6 +5,6 @@ description      "Installs/Configures round-three"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-%w{ application application_ruby postgresql git xml ruby-193 }.each do |cb|
+%w{ application application_ruby database rbenv postgresql git xml }.each do |cb|
   depends cb
 end
--- site-cookbooks/round-three/recipes/default.rb
+++ site-cookbooks/round-three/recipes/default.rb
@@ -6,11 +6,67 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+require 'rubygems'
+Gem.clear_paths
+require 'pg'
+
 require_recipe "git"
 require_recipe "postgresql::server"
 require_recipe "xml"
-require_recipe "ruby-193"
 round_three_secrets = Chef::EncryptedDataBagItem.load("apps", "round-three")
+postgresql_connection = {:host => "127.0.0.1",
+                         :port => 5432,
+                         :username => 'postgres',
+                         :password => node['postgresql']['password']['postgres']}
+
+postgresql_database 'round_three_production' do
+  connection postgresql_connection
+  action :create
+  owner 'round_three'
+  action :nothing
+end
+
+ruby_block "change-rt-db-password" do
+  block do
+    c = PGconn.connect(:user=> postgresql_connection[:username],
+                       :dbname=>'postgres',
+                       :host =>  postgresql_connection[:host],
+                       :password=> postgresql_connection[:password])
+    c.exec("ALTER ROLE round_three ENCRYPTED PASSWORD '#{round_three_secrets['passwords']['database']}'")
+  end
+  not_if do
+    begin
+      c = PGconn.connect(:user=> 'round_three',
+                         :dbname=> 'round_three_production',
+                         :host => postgresql_connection[:host],
+                         :password=> round_three_secrets['passwords']['database'])
+    rescue PGError
+      false
+    end
+  end
+  action :nothing
+end
+
+execute "create-rt-database-user" do
+  user 'postgres'
+  command "createuser -U #{postgresql_connection[:username]} -SDRw round_three"
+  only_if do
+    c = PGconn.connect(:user=> postgresql_connection[:username],
+                       :dbname=>'postgres',
+                       :host => postgresql_connection[:host],
+                       :password=> postgresql_connection[:password])
+    r = c.exec("SELECT COUNT(*) FROM pg_user WHERE usename='round_three'")
+    r.entries[0]['count'].to_i == 0
+  end
+  notifies :create, "postgresql_database[round_three_production]", :immediately
+  notifies :create, "ruby_block[change-rt-db-password]", :immediately
+end
+
+# Install the gem here, so the "passenger-install-apache-module" gets rehashed
+# It will actually be installed by the sub-resource below
+rbenv_gem "passenger" do
+  version node['passenger']['version']
+end
 
 application "round-three" do
   path "/srv/round-three"
@@ -26,10 +82,12 @@ application "round-three" do
 
   rails do
     gems ['bundler']
+    bundler_deployment false
     database do
       host    'localhost'
       adapter 'postgresql'
-      username 'round-three'
+      database 'round_three_production'
+      username 'round_three'
       password round_three_secrets['passwords']['database']
     end
   end
