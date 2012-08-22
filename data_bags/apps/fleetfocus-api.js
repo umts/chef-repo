6d0573c08e0f0a66e4c9fc33bc1a3770d22e5626
Matt Moretti <werebus@gmail.com>
2012-08-22 14:56:33 -0400

    Fleet focus API app using thin

---
+++ data_bags/apps/fleetfocus-api.js
@@ -0,0 +1,7 @@
+{
+  "dbname": "z4N1rWcPTLRAMK1FXKz3EQ==\n",
+  "id": "fleetfocus-api",
+  "dbpassword": "P9PPUItBv6/S5zMcpV6B8heOUaNLPb4+2uc/7D8keGw=\n",
+  "dbuser": "P9PPUItBv6/S5zMcpV6B8heOUaNLPb4+2uc/7D8keGw=\n",
+  "dbhost": "UKCJM/Rz7RYxDTG+EBGJfN790Sg7orXkWA6F5G3dnjKve+c/xoyIMy5YNiGp\nPK3N\n"
+}
---
+++ site-cookbooks/fuelfocus-api/recipes/default.rb
@@ -0,0 +1,70 @@
+#
+# Cookbook Name:: fuelfocus-api
+# Recipe:: default
+#
+# Copyright 2012, UMass Transit Service
+#
+# All rights reserved - Do Not Redistribute
+#
+require_recipe "git"
+require_recipe "build-essential"
+
+ff_secrets = Chef::EncryptedDataBagItem.load('apps', 'fleetfocus-api')
+deploy_path = "/srv/fleetfocus-api"
+
+application "fleetfocus-api" do
+  path deploy_path
+  repository "git://github.com/umts/fleetfocus-api.git"
+
+  owner 'root'
+  group 'root'
+  packages %w{ freetds-dev }
+
+  symlinks({"pids" => "tmp/pids", "log" => "log", "thin.yml" => "config/thin.yml"})
+  create_dirs_before_symlink ['tmp']
+
+  # "rails" for purposes of db config and vendoring bundle
+  rails do
+    gems ['bundler']
+    bundler_deployment false
+    database do
+      adapter 'sqlserver'
+      host     ff_secrets['dbhost']
+      database ff_secrets['dbname']
+      username ff_secrets['dbuser']
+      password ff_secrets['dbpassword']
+    end
+  end
+
+  before_symlink do
+    template "#{deploy_path}/shared/thin.yml" do
+      mode "0755"
+      variables(
+        :dir => "#{deploy_path}/current/",
+        :port => 4567,
+        :environment => node.chef_environment
+      )
+    end
+
+    %w{log pids}.each do |dir|
+      directory "#{deploy_path}/shared/#{dir}" do
+        mode "0755"
+      end
+    end
+  end
+end
+
+template "/etc/init.d/fleetfocus-api" do
+  source "thin-initd.erb"
+  mode   "0755"
+  variables(
+    :app_path => "#{deploy_path}/current",
+    :config => "#{deploy_path}/current/config/thin.yml",
+    :bundler => "/usr/local/rbenv/shims/bundle"
+  )
+end
+
+service "fleetfocus-api" do
+  supports :restart => true
+  action [:enable, :start]
+end
