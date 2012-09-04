ae309e4dc21f80772edfa0c0442a07ba883aa327
Matt Moretti <werebus@gmail.com>
2012-09-04 16:24:28 -0400

    My new key

--- data_bags/users/matt.js
+++ data_bags/users/matt.js
@@ -1,6 +1,6 @@
 {
   "id": "matt",
-  "ssh_keys": "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAuzJRAfVvsbl2vvWVSxKd4vKgXIxNqFw+27h9TDO4jLTWNydCbKjQwE3cxHZK8nbYphYk+5Iovpr1w+99QUbBudFHoAYN3G5qKgO7+X5wnrIpXnZgY/9k5Kqk16oD6W5qEHeswuOj8S2Zr+x1L0pNsXitHz7Gt3d846ybHi7c0QWcFj95vsZ0MacgPUM8pa2HkNiqKXwkGg2DPfCfk+m0ndkYl3+1kS/4yj5PL2vpPk0ZddqrFsdg6vcoYzOvMygJ7MbPGKXuN+YQUFu+JdOSriV0iWy2Tn2niiaG5LfBO1HN8rxBAkNKtyFbOvuxeAtKJzbj+mgtUZ9FQUTeZ2qZXQ== matt",
+  "ssh_keys": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDn7SzPWH5fv153uUxoOyOk7XgUJtbU7MbSYmqaFVotN9YmJswx9aY3seyjj5psepW6m2JZmmarUF+kA+ac2uGLj8tCHKS2yPGGZyXOB3J3tvsc9to7j4qPkvp/JZJYUAUoAVl4EZO7UmhrTvVMkHsooIXY6MCkiz432iktiTzKDb21LsSfb+tWrhMrKV9HSeSiUesWpBUsVLQIpM5czNMqH4Z/JPbBpebsAgaf7Y+PhsTzJRYNL9FTDui1IAIw3sVSRklFsyauCE2zv2IKCTNprJ73OJQQgIAEDFpYezfipG7E4hlr8ndc/EyGGSEZtfIfApj9AtclDT11wbhh/7Al matt",
   "groups": "sysadmin",
   "uid": 2001,
   "shell": "\/bin\/bash",
