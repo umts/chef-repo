8eef8adab9276512510a19a3ab4bce5108cb6660
Matt Moretti <werebus@gmail.com>
2012-10-30 14:28:05 -0400

    First pass at experimenting with ssl.
    The gpg-keys data bag is likely to be canned some time in the future.
    I'd like to modify the SSL cookbook to use encrypted data-bags instead
    of GPG.
    
    The ssl-test cookbook will go away as well, but something like it
    should become rolled into our infra-structure anyways.

---
+++ site-cookbooks/ssl-test/attributes/default.rb
@@ -0,0 +1,8 @@
+set['ssl']['key_vault'] = 'transit-mis@admin.umass.edu'
+
+set['ssl']['country'] = 'US'
+set['ssl']['state'] = 'MA'
+set['ssl']['city'] = 'Amherst'
+set['ssl']['organization'] = 'UMass Transit Service'
+set['ssl']['department'] = 'MIS'
+set['ssl']['email'] = 'transit-mis@admin.umass.edu'
---
+++ site-cookbooks/ssl-test/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "UMass Transit Service"
+maintainer_email "transit-mis@admin.umass.edu"
+license          "MIT"
+description      "Installs/Configures ssl-test"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+depends "ssl"
+depends "apache2"
