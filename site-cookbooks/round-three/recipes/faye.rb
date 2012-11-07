ef8d10eb4b8b4fd715a12ef713014f0daa2c98da
Brandon Sprague <brandon00sprague@gmail.com>
2012-11-07 16:02:00 -0500

    Added Upstart script

--- site-cookbooks/round-three/recipes/faye.rb
+++ site-cookbooks/round-three/recipes/faye.rb
@@ -7,8 +7,17 @@
 # All rights reserved - Do Not Redistribute
 #
 
+template "/etc/init.d/faye.conf" do
+  source "faye.conf.erb"
+  mode   "0755"
+  variables(
+    :round_three_dir => node['round-three']['dir'],
+    :faye_port => node['round-three']['faye-port'],
+    :bundler => node['rbenv']['root_path']
+  )
+end
+
 service "faye" do
-	start_command "#{node['rbenv']['root_path']}/shims/bundle exec rackup #{node['round-three']['dir']}/current/private_pub.ru -s thin -E production"
 	case node[:platform]
   when "ubuntu"
     if node[:platform_version].to_f >= 9.10
