e8646de22e90838e047521a5b71b268fa283c7a3
Matt Moretti <werebus@gmail.com>
2012-11-13 14:19:13 -0500

    Made some tweaks to the `knife.rb`
    * Added an "encrypted_data_bag_secret" config (for bootstrapping)
    * Used a variable for "home" to simplify syntax
    * Used `File.join` for cross-compat (and to complicate the syntax)

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,19 +1,22 @@
 require 'librarian/chef/integration/knife'
 
-current_dir =            File.dirname(__FILE__)
-user =                   ENV['CHEF_SERVER_USER'] || ENV['USER']
-orgname =                ENV['ORGNAME'] || 'umts'
+current_dir =             File.dirname(__FILE__)
+home =                    ENV['HOME']
+user =                    ENV['CHEF_SERVER_USER'] || ENV['USER']
+orgname =                 ENV['ORGNAME'] || 'umts'
 
-log_level                :info
-log_location             STDOUT
-node_name                user
-client_key               "#{ENV['HOME']}/.chef/#{user}.pem"
-validation_client_name   "#{orgname}-validator"
-validation_key           "#{ENV['HOME']}/.chef/#{orgname}-validator.pem"
-chef_server_url          "https://api.opscode.com/organizations/#{orgname}"
-cache_type               'BasicFile'
-cache_options( :path =>  "#{ENV['HOME']}/.chef/checksums" )
-cookbook_path            Librarian::Chef.install_path
+log_level                 :info
+log_location              STDOUT
+node_name                 user
+client_key                File.join(home, '.chef', "#{user}.pem")
+validation_client_name    "#{orgname}-validator"
+validation_key            File.join(home, '.chef', "#{orgname}-validator.pem")
+validation_key            "#{ENV['HOME']}/.chef/#{orgname}-validator.pem"
+chef_server_url           "https://api.opscode.com/organizations/#{orgname}"
+cache_type                'BasicFile'
+cache_options( :path =>   File.join(home, '.chef', 'checksums'))
+cookbook_path             Librarian::Chef.install_path
+encrypted_data_bag_secret File.join(current_dir, '..', 'encrypted_data_bag_secret')
 
 cookbook_copyright       "UMass Transit Service"
 cookbook_email           "transit-mis@admin.umass.edu"
