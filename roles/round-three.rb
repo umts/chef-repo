b7523e8e3961972447a9e13cdd0d55eb0f46305e
Matt Moretti <werebus@gmail.com>
2012-11-14 11:15:47 -0500

    Added env_run_lists to round-three role

--- roles/round-three.rb
+++ roles/round-three.rb
@@ -1,8 +1,17 @@
 name "round-three"
 description "umasstransit.org 'round-three'.  Our core web-app"
 
-run_list(
+# Everything but SSL
+base = [
   "role[round-three-base]",
   "recipe[round-three::faye]",
   "recipe[fuelfocus-api]"
+]
+
+run_list base
+
+env_run_lists(
+  "_default" => base,
+  "development" => base,
+  "production" => base + ["recipe[round-three::ssl]"]
 )
