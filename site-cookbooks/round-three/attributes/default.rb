22447811aa33ef71c41a981037c69407d7913b09
Matt Moretti <werebus@gmail.com>
2012-12-24 09:56:18 -0500

    Switch to production IdP, remove failing virtualhost

--- site-cookbooks/round-three/attributes/default.rb
+++ site-cookbooks/round-three/attributes/default.rb
@@ -1,3 +1,6 @@
 set['passenger']['version'] = "3.0.15"
+
+set['shibboleth']['idp'] = "https://webauth.oit.umass.edu"
+
 default['round-three']['dir'] = "/srv/round-three"
 default['round-three']['faye-port'] = 9292
