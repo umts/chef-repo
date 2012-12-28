c93b4c653e95f9404dd63eed06ab3bd1c06f1c72
Matt Moretti <werebus@gmail.com>
2012-12-28 13:46:28 -0500

    Attributes for Shibboleth

--- site-cookbooks/shibboleth/attributes/default.rb
+++ site-cookbooks/shibboleth/attributes/default.rb
@@ -1,1 +1,16 @@
-default['shibboleth']['idp'] = "https://shibbotest.oit.umass.edu"
+default['shibboleth']['idp'] = "https://idp.testshib.org"
+default['shibboleth']['remote_user'] = "eppn persistent-id targeted-id"
+
+default['shibboleth']['sessions']['lifetime'] = 28800
+default['shibboleth']['sessions']['timeout'] = 3600
+default['shibboleth']['sessions']['check_address'] = false
+default['shibboleth']['sessions']['handler_ssl'] = false
+
+default['shibboleth']['handlers']['metadata_generator'] = true
+default['shibboleth']['handlers']['status'] = true
+default['shibboleth']['handlers']['session'] = true
+default['shibboleth']['handlers']['discovery_feed'] = true
+
+default['shibboleth']['errors']['contact'] = "root@#{node['fqdn']}"
+default['shibboleth']['errors']['logo'] = '/shibboleth-sp/logo.jpg'
+default['shibboleth']['errors']['css'] = '/shibboleth-sp/main.css'
