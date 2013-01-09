1defd93f065573c54e9e721a5d42e6d1ac02a50e
Matt Moretti <werebus@gmail.com>
2013-01-09 18:12:14 -0500

    Added ssl intermediate chain files

--- site-cookbooks/round-three/recipes/ssl.rb
+++ site-cookbooks/round-three/recipes/ssl.rb
@@ -20,6 +20,7 @@ web_app "round-three-ssl" do
   rails_env       node.chef_environment =~ /_default/ ? "production" : node.chef_environment
   ssl_key         "/etc/ssl/#{domain}.key"
   ssl_certificate "/etc/ssl/#{domain}.cert"
+  ssl_chain       "/etc/ssl/in-common-imtermediate.cert"
 end
 
 apache_site "round-three.conf" do
@@ -32,4 +33,9 @@ ssl_certificate "transit-demo" do
   key         "/etc/ssl/#{domain}.key"
   certificate "/etc/ssl/#{domain}.cert"
   days        365
+end
+
+cookbook_file "/etc/ssl/in-common-intermediate.cert" do
+  mode '0644'
+  notifies :restart, 'service[apache2]'
 end
