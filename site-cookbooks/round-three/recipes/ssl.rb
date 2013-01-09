d2b27eb6500099fcd9a3535f2c873a66e063bda0
Matt Moretti <werebus@gmail.com>
2013-01-09 18:58:17 -0500

    Minor typo in ssl recipe

--- site-cookbooks/round-three/recipes/ssl.rb
+++ site-cookbooks/round-three/recipes/ssl.rb
@@ -11,22 +11,6 @@ include_recipe "apache2::mod_ssl"
 
 domain = "transit-demo.admin.umass.edu"
 
-web_app "round-three-ssl" do
-  template        "round-three-ssl.conf.erb"
-  docroot         "#{node['round-three']['dir']}/current/public"
-  server_name     "round-three.#{node['domain']}"
-  server_aliases  ["demo.umasstransit.org", "transit-demo.admin.umass.edu"]
-  log_dir         node['apache']['log_dir']
-  rails_env       node.chef_environment =~ /_default/ ? "production" : node.chef_environment
-  ssl_key         "/etc/ssl/#{domain}.key"
-  ssl_certificate "/etc/ssl/#{domain}.cert"
-  ssl_chain       "/etc/ssl/in-common-imtermediate.cert"
-end
-
-apache_site "round-three.conf" do
-  enable false
-end
-
 ssl_certificate "transit-demo" do
   name        domain
   ca          "InCommon"
@@ -39,3 +23,20 @@ cookbook_file "/etc/ssl/in-common-intermediate.cert" do
   mode '0644'
   notifies :restart, 'service[apache2]'
 end
+
+web_app "round-three-ssl" do
+  template        "round-three-ssl.conf.erb"
+  docroot         "#{node['round-three']['dir']}/current/public"
+  server_name     "round-three.#{node['domain']}"
+  server_aliases  ["demo.umasstransit.org", "transit-demo.admin.umass.edu"]
+  log_dir         node['apache']['log_dir']
+  rails_env       node.chef_environment =~ /_default/ ? "production" : node.chef_environment
+  ssl_key         "/etc/ssl/#{domain}.key"
+  ssl_certificate "/etc/ssl/#{domain}.cert"
+  ssl_chain       "/etc/ssl/in-common-intermediate.cert"
+end
+
+apache_site "round-three.conf" do
+  enable false
+end
+
