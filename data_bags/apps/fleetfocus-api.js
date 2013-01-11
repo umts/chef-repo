57800c80ea2e1d67c23fe4e60f36c25006c243e1
Matt Moretti <werebus@gmail.com>
2013-01-11 11:24:21 -0500

    Updated FA db location

--- data_bags/apps/fleetfocus-api.js
+++ data_bags/apps/fleetfocus-api.js
@@ -1,7 +1,7 @@
 {
   "dbname": "z4N1rWcPTLRAMK1FXKz3EQ==\n",
   "id": "fleetfocus-api",
-  "dbpassword": "P9PPUItBv6/S5zMcpV6B8heOUaNLPb4+2uc/7D8keGw=\n",
-  "dbuser": "P9PPUItBv6/S5zMcpV6B8heOUaNLPb4+2uc/7D8keGw=\n",
-  "dbhost": "UKCJM/Rz7RYxDTG+EBGJfN790Sg7orXkWA6F5G3dnjKve+c/xoyIMy5YNiGp\nPK3N\n"
+  "dbpassword": "ubfZ/DS7dQouMH9ZXFmYbItJWPNAK/mKaKhNJ/m/fZU=\n",
+  "dbuser": "a6tkIJ0F2O9/GJBNxztQVR1zurogEQz/5mplUlo+IAk=\n",
+  "dbhost": "JiF8RRvXDOOZMizYElBztUHtANd93VJveLxo9y6WZSortwZuEsm0nReIp37A\nqqhw\n"
 }
