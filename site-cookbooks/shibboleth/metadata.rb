ed6867d5f1b293ea99daeb97228b276c1c375ccd
Matt Moretti <werebus@gmail.com>
2013-01-11 15:46:00 -0500

    Moved Shibboleth out to its own git repo

--- site-cookbooks/shibboleth/metadata.rb
+++
@@ -1,9 +0,0 @@
-maintainer       "UMass Transit Service"
-maintainer_email "transit-mis@admin.umass.edu"
-license          "MIT"
-description      "Installs/Configures shibboleth"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
-
-depends "apache2"
-supports "ubuntu"
