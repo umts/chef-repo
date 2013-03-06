7833625c48dffd5bd51bcee9745972e7a7523fff
Brandon Sprague <brandon00sprague@gmail.com>
2013-03-06 15:50:35 -0500

    New ssh key

--- data_bags/users/brandon.js
+++ data_bags/users/brandon.js
@@ -1,6 +1,6 @@
 {
   "id": "brandon",
-  "ssh_keys": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDmKr5SKpif01RlgtcfgZEkK9qUB/p/rWmH3+cDHznU47D82+BywDNLGhFvyLlBvNBupwQEs9mYGKg5K2kv0FUMhEtRxTC0uXZbXhBNnt4qh3o2VZjlLdbW2Fqwed27DivziP1qlI0YU++7KWbYjUxPBPYGby9tLM7L7xZRmPdBYnQEXbXL79O/wmxKtvSYLq3DvvpulJ3InOZ7kLpr0nDxkpbVN5bz4hlWUmma6AR8BqQFaif6GRwxpGvdmdRbrci17vopp+dClDnHLjawb44R9UbRD8i2leMxbR7XSI4n4blZKcwXRPt8XJ7chwGbGEv0W9A+GUBvzWjLNnjhBSwn brandon00sprague@gmail.com",
+  "ssh_keys": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDO1lSrvXpqXjSBJ///teqzUYb+51BjWUC2eG/+JkMDLqtu/5gS1W8Yhd7G2toTG5ZTgK9b7sY2u8DtJ/Y8GFZbOS2ulqabkT7AoT786HRzlYCSJpS/UB4ulsqrxyGz65wdftMdktqyd9CExLCpxWGlvHiMCtegCZ4WxWHIehxjrRqu8qOOJ7IeyPs6zp+UBVR0xnOIjZItbmpq2iAZA4iiEcoGIR3LcwW0eBROiy2iOJBSbLwyXWNrziQoZazwizt8d73VWdcuzfd8P9kaTCuOo11UVYfUmuFb7iESh1VO/+KcF0FRZtlXgox7nLCMKc5YjXQ+HD5hvyjG0+K0srLd brandon00sprague@gmail.com",
   "groups": "sysadmin",
   "uid": 2006,
   "shell": "\/bin\/bash",
