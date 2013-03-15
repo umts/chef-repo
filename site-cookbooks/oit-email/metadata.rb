15d0adf147bc651e7a9db99fac9301dc8cc934f9
Matt Moretti <werebus@gmail.com>
2013-03-15 10:43:42 -0400

    Changed the oit-email from a role to a wrapper cookbook.
    I hate to join in on the role-hate out there but...

---
+++ site-cookbooks/oit-email/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "UMass Transit Service"
+maintainer_email "transit-mis@admin.umass.edu"
+license          "MIT"
+description      "Installs/Configures oit-email"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+depends "ssmtp"
