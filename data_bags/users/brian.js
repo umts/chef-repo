c742b11eba42cf783497603c28659eddbb2e8811
Matt Moretti <werebus@gmail.com>
2013-04-03 10:38:20 -0400

    Added Franky, removed Logan

---
+++ data_bags/users/brian.js
@@ -0,0 +1,8 @@
+{
+  "id": "brian",
+  "ssh_keys": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDUilej4q2+92ax0tQbgwol+FVU2Gy/iJfbSdN1quiZcuJEJtE61rhbDInT6qLkRGexRORBhtAbhmt8uMDniqTIK3owyHQvF9rKJZoUZkoFqsn1aZIDU8hocuuEMXwdndxer7b3hSJkYqeB4s+0OoWJBFBbxHcMthm59YBHPR7nCv3P3XzwQU7ONWEI2vXxilGO1ywGXR+5jpUQR1rKIrax4f2eimY824fXbIobMMiP+37G/vOdU/SKn+x/WzmJcj1J2WIU3FdMJSTuLBowetMgZ7fCgikdes4LhOZj3GkAfH9yiTIPnQY3867WlXmtGV5eEZ5fkO9QrwKZM4iwUfmL thebluenu@gmail.com",
+  "groups": "sysadmin",
+  "uid": 2008,
+  "shell": "\/bin\/bash",
+  "comment": "Brian Francis"
+}
--- data_bags/users/logan.js
+++
@@ -1,8 +0,0 @@
-{
-  "id": "logan",
-  "ssh_keys": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCwCmRNLR9qeruQ07Mr/WwOlJB6+H69FTUAMa3mhVz3a+kHI8dhF1C2t0hiQ3RrQcqWUj37G5ET9pvr/c3JUjYxHXYAhoMbcqPL3Uk6p3hQw6j+m4Mx7KfmvG1ScW7/ZUmlakiWb5hs3fTWmzRTa9ryALT/oRBnxDWd1frRVpLTt3bLzuqUzAYSRyfINKBNjNLrQKSmEcA5dzFAM/qJ9oOxtVvkL/8dYHxG/JrNTmyP9+PjusckpL+uhV2msNhXF5HQPHCaS1kb6NBPGmruxHhPIkl3z70z5yozgY8YOrtb/XpVeky2cOA9JqE7qUBBzvrqlL9nhcfsnbUPnSlRufsd",
-  "groups": "sysadmin",
-  "uid": 2003,
-  "shell": "\/bin\/bash",
-  "comment": "G. Logan Slinski"
-}
