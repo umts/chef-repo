1da23dc3047aaf2c63f6aecf7980062e40a88d85
Matt Moretti <werebus@gmail.com>
2013-05-11 10:24:35 -0400

    Bad at JSON

--- data_bags/users/tyler.js
+++ data_bags/users/tyler.js
@@ -1,6 +1,6 @@
 {
   "id": "tyler",
-  "ssh_keys": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCdbn5EjFRzb2W2lckn+7VBcxR3WHCaK7Guk/Bn2ieLOUKtENUwO4pNY+W7dimLrU3RCjuTP0AwRi38QSy4SjmhysTqKisMrxRptHc4zRUJffRZk5pbP1Ur275XicZyBnReeV8WKB1AG9teBCeO1hSZeOT/Fa9dEGv9JAa+UQvpZrbX5LmRbCy2muh/OlB3jNCGz9DsMVxCmoD9nJOmlQmrLPfvzLHFcntrkJZp6zPHYiVapMZZ9mCDhEIra3XoDdng9gB+iQWiQxWiZi0JdNurbSSZQDmInlZqX1aMCvtz98eBi9wqvdSUqHdkXK+xf5OmUaItz+ZRuehUooKR9nut"
+  "ssh_keys": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCdbn5EjFRzb2W2lckn+7VBcxR3WHCaK7Guk/Bn2ieLOUKtENUwO4pNY+W7dimLrU3RCjuTP0AwRi38QSy4SjmhysTqKisMrxRptHc4zRUJffRZk5pbP1Ur275XicZyBnReeV8WKB1AG9teBCeO1hSZeOT/Fa9dEGv9JAa+UQvpZrbX5LmRbCy2muh/OlB3jNCGz9DsMVxCmoD9nJOmlQmrLPfvzLHFcntrkJZp6zPHYiVapMZZ9mCDhEIra3XoDdng9gB+iQWiQxWiZi0JdNurbSSZQDmInlZqX1aMCvtz98eBi9wqvdSUqHdkXK+xf5OmUaItz+ZRuehUooKR9nut",
   "groups": "developers",
   "uid": 2008,
   "shell": "\/bin\/bash",
