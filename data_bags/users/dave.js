e918a0830b7cc67e1c2452b222909142963a8ba4
Matt Moretti <werebus@gmail.com>
2013-05-16 14:18:32 -0400

    Added Dave

---
+++ data_bags/users/dave.js
@@ -0,0 +1,8 @@
+{
+  "id": "dave",
+  "ssh_keys": "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA+Iixu2ap7skpkzABN22P389+eXV60cbvRqsnBdGznsMAIohiu2bI4jP1QAIvjcwc9ZyJc4wze8FgDBdFpJncyzkDC6HmeFzWeOJ9FUzPp5dHYludk7RN/LEW7k/HmHLGifdzAANXmSsrdcpWqeB1u1hQW+iKNBk3lKwcdpnvJbU01OVKRxTPz5v1MhuRHHsQcywQXLvyVgk/vpMn+Hv/UEdDoYTe7rVEp3xI73KJwpdjmE06Wvu0PZMYpurtJpdNIFAHSr9JtU6hIgETisHaOidC+d+/7RKItmTo0BPBA3uyXiaUoyjsHLIgt7DxkQlro8LloiL2yWYNvrFKcFWqRw==",
+  "groups": "developers",
+  "uid": 2012,
+  "shell": "\/bin\/bash",
+  "comment": "Dave Faulkenberry"
+}
