a1ec2bb914ee958834858f2b51b6b03dd8d3d8fc
Matt Moretti <werebus@gmail.com>
2013-06-07 12:08:23 -0400

    rt 0.4

--- environments/production.rb
+++ environments/production.rb
@@ -1,5 +1,5 @@
 name        "production"
 description "Production environment - for servers"
 
-cookbook "round-three", "= 0.0.3"
+cookbook "round-three", "= 0.0.4"
 cookbook "postgresql", "~> 0.99"
