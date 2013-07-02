9cbbf9f0fba602f3bc90c8f05fe5f46af97b5a01
Matt Moretti <werebus@gmail.com>
2013-07-02 19:44:05 -0400

    Alright, let's do this

--- environments/production.rb
+++ environments/production.rb
@@ -2,5 +2,5 @@ name        "production"
 description "Production environment - for servers"
 
 cookbook "round-three", "= 0.0.4"
-cookbook "shibboleth", "= 0.0.1"
+cookbook "shibboleth", "= 0.0.2"
 cookbook "postgresql", "~> 0.99"
