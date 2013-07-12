4d825dc41bd292fd59ea73b1a35ceed5b0187cfd
Matt Moretti <werebus@gmail.com>
2013-07-12 10:45:58 -0400

    New key for tylere

--- data_bags/users/tyler.js
+++ data_bags/users/tyler.js
@@ -1,6 +1,6 @@
 {
   "id": "tyler",
-  "ssh_keys": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCdbn5EjFRzb2W2lckn+7VBcxR3WHCaK7Guk/Bn2ieLOUKtENUwO4pNY+W7dimLrU3RCjuTP0AwRi38QSy4SjmhysTqKisMrxRptHc4zRUJffRZk5pbP1Ur275XicZyBnReeV8WKB1AG9teBCeO1hSZeOT/Fa9dEGv9JAa+UQvpZrbX5LmRbCy2muh/OlB3jNCGz9DsMVxCmoD9nJOmlQmrLPfvzLHFcntrkJZp6zPHYiVapMZZ9mCDhEIra3XoDdng9gB+iQWiQxWiZi0JdNurbSSZQDmInlZqX1aMCvtz98eBi9wqvdSUqHdkXK+xf5OmUaItz+ZRuehUooKR9nut",
+  "ssh_keys": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBtis+CiNGuxIiyKGUdB1Q+7mtiunYkpqd/97TmUk+Sa6mTYhnUMUWyr5DJGnKkuuDwMa6O7+vloM7m2NDDSO5Feh9idFsvtTouaTlzD+xeUAeh147vORkcCrqe9V5odXmBGG+2fn1sv/CKgp5vX+kXElNhf1rO3NGjWxyKAwCJ/i6BkvAO14foLjEJTxx0bnJnsKv+iRp9j/Zn11FmKwADHXJsOR0jN8V0E3bUtlAEREiIwDsFQVjcon+uwjhAV9I6VVUqekE4wfqkPd5HeTRBu99Y0Nd2VZ8DjPPS6iNI8Xz951wS2sju7rqLRLphN5Ao6Z8uARJH9E5oGoGOPkX",
   "groups": "developers",
   "uid": 2009,
   "shell": "\/bin\/bash",
