65c9f8316dda8317f10f0bb84e44bc43a8ef899e
Matt Moretti <werebus@gmail.com>
2013-07-22 15:32:32 -0400

    Postgres fix is in, also deploy group

--- environments/production.rb
+++ environments/production.rb
@@ -1,6 +1,6 @@
 name        "production"
 description "Production environment - for servers"
 
-cookbook "round-three", "= 0.0.6"
+cookbook "round-three", "= 0.0.7"
 cookbook "shibboleth", "= 0.0.2"
-cookbook "postgresql", "~> 0.99"
+cookbook "postgresql", "~> 3.0"
