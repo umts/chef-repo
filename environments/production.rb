f2e87fd3abe22446762ba1ececc3e041460abe6f
Matt Moretti <werebus@gmail.com>
2013-07-23 08:15:09 -0400

    Just kidding, 2.2 is fine for pg

--- environments/production.rb
+++ environments/production.rb
@@ -3,4 +3,4 @@ description "Production environment - for servers"
 
 cookbook "round-three", "= 0.0.7"
 cookbook "shibboleth", "= 0.0.2"
-cookbook "postgresql", "~> 3.0"
+cookbook "postgresql", "~> 2.2"
