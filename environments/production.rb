a8b3032c1427b7ff1b5ec258baadfb827ece5d6c
Matt Moretti <werebus@gmail.com>
2013-08-09 16:38:20 -0400

    Bump

--- environments/production.rb
+++ environments/production.rb
@@ -1,6 +1,6 @@
 name        "production"
 description "Production environment - for servers"
 
-cookbook "round-three", "= 0.0.9"
+cookbook "round-three", "= 0.0.12"
 cookbook "shibboleth", "= 0.0.2"
 cookbook "postgresql", "~> 2.2"
