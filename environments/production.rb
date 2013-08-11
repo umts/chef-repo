c5024398cfdad89ac215e1ff99b03bd226801217
Matt Moretti <werebus@gmail.com>
2013-08-11 12:27:44 -0400

    Bump for ssl fix

--- environments/production.rb
+++ environments/production.rb
@@ -1,6 +1,6 @@
 name        "production"
 description "Production environment - for servers"
 
-cookbook "round-three", "= 0.0.12"
+cookbook "round-three", "= 0.0.13"
 cookbook "shibboleth", "= 0.0.2"
 cookbook "postgresql", "~> 2.2"
