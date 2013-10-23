04192cd57df5c5b7559bb4e8b5e117eec1147b3b
Matt Moretti <werebus@gmail.com>
2013-10-23 13:31:33 -0400

    'Revoke' Dave's key; new key pending

--- data_bags/users/dave.js
+++ data_bags/users/dave.js
@@ -1,6 +1,6 @@
 {
   "id": "dave",
-  "ssh_keys": "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA+Iixu2ap7skpkzABN22P389+eXV60cbvRqsnBdGznsMAIohiu2bI4jP1QAIvjcwc9ZyJc4wze8FgDBdFpJncyzkDC6HmeFzWeOJ9FUzPp5dHYludk7RN/LEW7k/HmHLGifdzAANXmSsrdcpWqeB1u1hQW+iKNBk3lKwcdpnvJbU01OVKRxTPz5v1MhuRHHsQcywQXLvyVgk/vpMn+Hv/UEdDoYTe7rVEp3xI73KJwpdjmE06Wvu0PZMYpurtJpdNIFAHSr9JtU6hIgETisHaOidC+d+/7RKItmTo0BPBA3uyXiaUoyjsHLIgt7DxkQlro8LloiL2yWYNvrFKcFWqRw==",
+  "ssh_keys": "",
   "groups": "developers",
   "uid": 2012,
   "shell": "\/bin\/bash",
