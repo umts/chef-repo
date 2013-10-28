de0725adf47597d5e3303aa8680441cf7286aba2
Matt Moretti <werebus@gmail.com>
2013-10-28 13:12:48 -0400

    New key for Dave

--- data_bags/users/dave.js
+++ data_bags/users/dave.js
@@ -1,6 +1,6 @@
 {
   "id": "dave",
-  "ssh_keys": "",
+  "ssh_keys": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCYBGBabIhogZtmJlg9zveKVXtmjX6vWSx6G8Mc8nma1DzTzH3X7AdAqpbDEfkCYZcZe+JyMV3YhyPWy+d+K6lTugF/chnblT/90zZ8OxldtkTaf69FdBNY7CgcVK9O6HGjfmdqB6po+LzscABYsZG8YXQ113VY5NbsnfruUUwL65H29pXJiVyCi1PRx2l6MvJX13oTdamdPm52p3HVC0HJuzKTjwjoZfh9NoyKfMoUdhCZl6OuyBD0pVaFlXHbfAd/m6UVqdFSj7FlxtqBuMsqB5PngboiDMXuVXAfAAf1uJooRZcDWHP2xNhJ7szsZYR6g9tVJFRuUxw2f7y3l7Mj",
   "groups": "developers",
   "uid": 2012,
   "shell": "\/bin\/bash",
