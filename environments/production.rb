6d225d031a1eeaa733a745eb187f1d630cc74001
Matt Moretti <werebus@gmail.com>
2013-12-26 15:31:17 -0500

    Bump round-three cb in prod

--- environments/production.rb
+++ environments/production.rb
@@ -1,6 +1,6 @@
 name        "production"
 description "Production environment - for servers"
 
-cookbook "round-three", "= 0.0.17"
+cookbook "round-three", "= 0.0.20"
 cookbook "shibboleth", "= 0.0.2"
 cookbook "postgresql", "~> 2.2"
