8204579f919d59ecdc54275a1b742b928117d1f7
Matt Moretti <werebus@gmail.com>
2014-01-14 11:37:46 -0500

    Added Mike (finally)

---
+++ data_bags/users/mike.js
@@ -0,0 +1,8 @@
+{
+  "id": "mike",
+  "ssh_keys": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDW5IbhAtygspOtGThtxW6xldV5YrHQyXlb0ud002xmxE2a3nBAmQMjV2borKB70HhWjM0igErsfTzBto0aW4It9ERArYheYHE8CoeUhqH8diPUf9upkg5knlho9J53jhxftchBQyNc3eVywYCg+CPfI/6EIfVj346yh64YvlArHYLUTTE31hHMS67SkEHo+tKdjIJc7F1hjFmGdrnbI30cjcSltOXjEuAVd18hHwNCinLWzF5nplDHL83APQH1YXZW+5K5SsoL6Yap/JGs4HzwoolOBkZlT3J9h3Z3YFmCislf8It1VyOkhxYB+vXgBmmXfGcdBNNwyuiZMgl+qyOv mikeCedrone503@gmail.com",
+  "groups": "developers",
+  "uid": 2013,
+  "shell": "\/bin\/bash",
+  "comment": "Mike Cedrone"
+}
