da6b9ecbec7cdc6cc2cdf19273ab34ca1899ff2e
Matt Moretti <werebus@gmail.com>
2015-03-23 13:26:06 -0400

    Some minor housecleaning

--- site-cookbooks/ruby-193/metadata.rb
+++
@@ -1,9 +0,0 @@
-maintainer       "UMass Transit Service"
-maintainer_email "transit-mis@admin.umass.edu"
-license          "All rights reserved"
-description      "Installs/Configures ruby 1.9.3"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
-
-depends 'ruby_build'
-depends 'rbenv'
